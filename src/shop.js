ecff898385428396013afb17c65ffee48ac4faba
Juanjo Molinero <jjmolinero@gmail.com>
2018-07-19 16:40:07 +0200

    Simplify design of the Shop class
    Now that we have tests, we can start to simplify the design of the
    problematic class. We do this by dividing the behaviour of the update
    quality method based on the type of item.

--- src/shop.js
+++ src/shop.js
@@ -4,49 +4,41 @@ class Shop {
   }
 
   updateQuality() {
+    function updateQualityNormal(item) {
+      item.sellIn -= 1;
+      item.quality -= 1;
+
+      if (item.sellIn < 0) item.quality -= 1;
+      if (item.quality < 0) item.quality = 0;
+    }
+
+    function updateQualityBackstagePasses(item) {
+      item.sellIn -= 1;
+
+      item.quality += 1;
+      if (item.sellIn < 10) item.quality += 1;
+      if (item.sellIn < 5) item.quality += 1;
+
+      if (item.quality > 50) item.quality = 50;
+      if (item.sellIn === -1) item.quality = 0;
+    }
+
+    function updateQualityAgedBrie(item) {
+      item.sellIn -= 1;
+      if (item.quality < 50) item.quality += 1;
+    }
+
     this.items.forEach((item) => {
-      if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-            item.quality -= 1;
-          }
-        }
-      } else if (item.quality < 50) {
-        item.quality += 1;
-        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality += 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality += 1;
-            }
-          }
-        }
-      }
-      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn -= 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name !== 'Aged Brie') {
-          if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-                item.quality -= 1;
-              }
-            }
-          } else {
-            item.quality -= item.quality;
-          }
-        } else if (item.quality < 50) {
-          item.quality += 1;
-        }
+      if (item.name === 'Aged Brie') {
+        updateQualityAgedBrie(item);
+      } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        // Nothing to be done in this case as Sulfuras does not modify its values
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        updateQualityBackstagePasses(item);
+      } else {
+        updateQualityNormal(item);
       }
     });
-
-    return this.items;
   }
 }
 
